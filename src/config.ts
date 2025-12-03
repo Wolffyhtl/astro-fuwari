f3df02436502955a2f64867d1f6f66d0912cdb3a
Wolffyhtl <matextsanzhedie@outlook.com>
2025-12-03 22:02:49 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -50,12 +50,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		LinkPreset.Friends,
-		{
-			name: "统计",
-			url: "https://cloud.umami.is/share/8DJaB5vQAtklYTpl", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
+		LinkPreset.Friends
 	],
 };
 
