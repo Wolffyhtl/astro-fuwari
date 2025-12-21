752f94bd27b35fa0a6f89770361d70cb5413a2c8
Wolffyhtl <matextsanzhedie@outlook.com>
2025-12-21 16:10:23 +0800

    Someed minor tweaks

---
+++ src/constants/icon.ts
@@ -0,0 +1,44 @@
+import type { Favicon } from "@/types/config.ts";
+
+export const defaultFavicons: Favicon[] = [
+	{
+		src: "/photos/photo32.png",
+		theme: "light",
+		sizes: "32x32",
+	},
+	{
+		src: "/photos/photo128.png",
+		theme: "light",
+		sizes: "128x128",
+	},
+	{
+		src: "/photos/photo180.png",
+		theme: "light",
+		sizes: "180x180",
+	},
+	{
+		src: "/photos/photo192.png",
+		theme: "light",
+		sizes: "192x192",
+	},
+	{
+		src: "/photos/photo32.png",
+		theme: "dark",
+		sizes: "32x32",
+	},
+	{
+		src: "/photos/photo128.png",
+		theme: "dark",
+		sizes: "128x128",
+	},
+	{
+		src: "/photos/photo180.png",
+		theme: "dark",
+		sizes: "180x180",
+	},
+	{
+		src: "/photos/photo192.png",
+		theme: "dark",
+		sizes: "192x192",
+	},
+];
