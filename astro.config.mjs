f554a7e041af2a95522d6e995281946490f0fc43
Clina <matextsanzhedie@outlook.com>
2026-01-03 14:34:30 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.150191.xyz/",
+	site: "https://cli.osfs.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
