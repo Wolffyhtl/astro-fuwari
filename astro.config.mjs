27274ef4381d25795223dda2cbc96f150332298a
Clina <admin@150191.xyz>
2026-01-13 19:01:22 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.150191.xyz/",
+	site: "https://blog.cli.gv.uy/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
