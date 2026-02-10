7c5d4588f63f493d28ab37744a14689ac3b6b1c4
Wolffyhtl <matextsanzhedie@outlook.com>
2026-02-10 15:53:30 +0800

    更改 EdgeOne Pages 使用域名

--- src/config.ts
+++ src/config.ts
@@ -49,7 +49,7 @@ export const siteConfig: SiteConfig = {
 	],
 	officialSites: [
 		{ url: "https://blog.150191.xyz", alias: "Netlify" },
-		{ url: "https://clina.webn.cc", alias: "EdgeOne Pages" },
+		{ url: "https://clina.zz.ac", alias: "EdgeOne Pages" },
 		{ url: "https://clina.sylu.cc", alias: "Codeberg Pages" },
 		{ url: "https://cli.osfs.top", alias: "GitHub Pages"},
 		{ url: "https://blog.cli.gv.uy", alias: "CloudFlare Workers"},
