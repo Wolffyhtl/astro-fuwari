5d487b349bed247a45ba697adcedfb5f42e5e616
clina_sama <184842861+Wolffyhtl@users.noreply.github.com>
2026-06-19 22:30:19 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.clina.top/",
+	site: "https://clina.top/",
 	base: "/",
 	trailingSlash: "always",
 	image: {
